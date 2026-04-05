090db71e6b79e794269df669be6cf1c052473ee9
Siddharthjha48 <145914732+Siddharthjha48@users.noreply.github.com>
2026-04-06 01:01:56 +0530

    Time: 4 ms (27.83%), Space: 10.2 MB (95.43%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int l=0,r=0,u=0,d=0;
+        for(int i=0;i<moves.length();i++){
+            char c = moves[i];
+            if(c == 'L'){
+
+                l++;
+            }
+            
+            else if(c == 'U'){
+                u++;
+            }
+            else if(c == 'R'){
+                r++;
+            }
+            else if(c == 'D'){
+                d++;
+            }
+            
+        }
+
+        if(u == d && r == l){
+            return true;
+        }
+     
+            return false ;
+        
+       
+        
+    }
+};
