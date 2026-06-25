edefa21886b78908d73743ec536a5f3399033d86
Siddharthjha48 <145914732+Siddharthjha48@users.noreply.github.com>
2026-06-25 16:27:58 +0530

    Time: 0 ms (100%), Space: 12.4 MB (9.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> merged;
-        int i=0,j=0;
+        int i = m - 1;         
+        int j = n - 1;         
+        int k = m + n - 1;     
 
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-                merged.push_back(nums1[i]);
-                i++;
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
             }
-            else if(nums2[j]<=nums1[i]){
-                merged.push_back(nums2[j]);
-                j++;
+            else {
+                nums1[k] = nums2[j];
+                j--;
             }
+            k--;
         }
 
-        while(i<m){
-            merged.push_back(nums1[i]);
-            i++;
-        }
-        while(j<n){
-            merged.push_back(nums2[j]);
-            j++;
-        }
-        
-
-        for(int k=0;k<merged.size();k++){
-            nums1[k] = merged[k] ;
+        while(j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 };
